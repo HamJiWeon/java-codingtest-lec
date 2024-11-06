610ecac36049dbdc26e806bf0d752ba720032fde
hamjiwon <skxo0039@gmail.com>
2024-11-06 16:59:14 +0900

    java-test-lec: 중복 문자 제거

---
+++ src/test1/Main6.java
@@ -0,0 +1,22 @@
+package test1;
+
+import java.util.Scanner;
+
+public class Main6 {
+    public String solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            // System.out.println(str.charAt(i)+ " " + i + " " + str.indexOf(str.charAt(i)));
+            if (str.indexOf(str.charAt(i)) == i) answer += str.charAt(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main6 main6 = new Main6();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(main6.solution(str));
+    }
+}
