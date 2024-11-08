cf893c0a8a14b4fb93db344abfcb5c3aa73e974a
hamjiwon <skxo0039@gmail.com>
2024-11-08 15:29:48 +0900

    java-test-lec: 가장 짧은 문자거리

---
+++ src/test1/Main10.java
@@ -0,0 +1,38 @@
+package test1;
+
+import java.util.Scanner;
+
+public class Main10 {
+    public int[] solution(String str, char ch) {
+        int[] answer = new int[str.length()];
+        int p = 1000;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+        p = 1000;
+        for (int i = str.length() - 1; i >= 0 ; i--) {
+            if (str.charAt(i) == ch) p = 0;
+            else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main10 main10 = new Main10();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        for (int x : main10.solution(str, c)) {
+            System.out.print(x + " ");
+        }
+    }
+}
