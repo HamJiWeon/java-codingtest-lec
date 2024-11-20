c746d7cdaf3ed2a6c0827f5e4ab41040a53c114d
hamjiwon <skxo0039@gmail.com>
2024-11-20 16:42:13 +0900

    java-test-lec: 선택 정렬

---
+++ src/test6/Main1.java
@@ -0,0 +1,29 @@
+package test6;
+
+import java.util.Scanner;
+
+public class Main1 {
+    public int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[index]) index = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main1 m = new Main1();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int x : m.solution(n, arr)) System.out.print(x + " ");
+    }
+}
